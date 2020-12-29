22be694c8ccddc809b91190696985b88c95db506
Любовь Макарова <arsenii07@gmail.com>
2020-12-29 10:12:02 +0300

    lesson29

--- script.js/main.js
+++ script.js/main.js
@@ -417,21 +417,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	const sendForm = () => {
 
-		const postData = body => new Promise((resolve, reject) => {
-			const request = new XMLHttpRequest();
-			request.addEventListener('readystatechange', () => {
-				if (request.readyState !== 4) {
-					return;
-				}
-				if (request.status === 200) {
-					resolve();
-				} else {
-					reject(request.status);
-				}
-			});
-			request.open('POST', 'server.php');
-			request.setRequestHeader('Content-Type', 'application/json');
-			request.send(JSON.stringify(body));
+		const postData = () => fetch('./server.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: 'formData'
 		});
 
 		const errorMessage = 'Что-то пошло не так...',
@@ -443,28 +434,24 @@ window.addEventListener('DOMContentLoaded', function() {
 		statusMessage.style.color = '#19b5fe';
 		statusMessage.style.margin = '25px';
 
-		const formSend = () => {
-			form.addEventListener('submit', event => {
-				event.preventDefault();
-				form.appendChild(statusMessage);
-				statusMessage.textContent = loadMessage;
-				const formData = new FormData(form);
+		form.addEventListener('submit', event => {
+			event.preventDefault();
+			form.appendChild(statusMessage);
+			const formData = new FormData(form);
+			statusMessage.textContent = loadMessage;
 
-				const body = {};
-				formData.forEach((val, key) => {
-					body[key] = val;
-				});
-				postData(body, () => {
+			postData(formData)
+				.then(response => {
+					if (response.status !== 200) {
+						throw new Error('status network not 200');
+					}
 					statusMessage.textContent = successMesage;
-				}, error => {
+				})
+				.catch(error => {
 					statusMessage.textContent = errorMessage;
 					console.error(error);
 				});
-			});
-		};
-		postData()
-			.then(formSend)
-			.catch(error => console.error(error));
+		});
 	};
 	sendForm();
 
