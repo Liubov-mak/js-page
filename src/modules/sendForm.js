dbe0df413b4696328e94f03aefa84289954588e1
Любовь Макарова <arsenii07@gmail.com>
2020-12-30 17:45:06 +0300

    lesson30

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -12,33 +12,43 @@ const sendForm = () => {
 		loadMessage = 'Загрузка...',
 		successMesage = 'Спасибо! Мы скоро с Вами свяжемся!';
 
-	const form = document.getElementById('form1');
+	const form1 = document.getElementById('form1'),
+		form2 = document.getElementById('form2'),
+		form3 = document.getElementById('form3');
+
+	const form = [form1, form2, form3];
+	console.log(form);
+
+
 	const statusMessage = document.createElement('div');
 	statusMessage.style.color = '#19b5fe';
 	statusMessage.style.margin = '25px';
 
-	form.addEventListener('submit', event => {
-		event.preventDefault();
-		form.appendChild(statusMessage);
-		const formData = new FormData(form);
-		statusMessage.textContent = loadMessage;
+	form.forEach(element => {
+		element.addEventListener('submit', event => {
+			event.preventDefault();
+			element.appendChild(statusMessage);
+			const formData = new FormData(element);
+			statusMessage.textContent = loadMessage;
 
-		postData(formData)
-			.then(response => {
-				if (response.status !== 200) {
-					throw new Error('status network not 200');
-				}
-				statusMessage.textContent = successMesage;
-			})
-			.catch(error => {
-				statusMessage.textContent = errorMessage;
-				console.error(error);
-			});
+			postData(formData)
+				.then(response => {
+					if (response.status !== 200) {
+						throw new Error('status network not 200');
+					}
+					statusMessage.textContent = successMesage;
+				})
+				.catch(error => {
+					statusMessage.textContent = errorMessage;
+					console.error(error);
+				});
 
-		setTimeout(() => {
-			form.querySelectorAll('input').forEach(element => { element.value = ''; });
-		}, 2000);
+			setTimeout(() => {
+				element.querySelectorAll('input').forEach(element => { element.value = ''; });
+			}, 1500);
+		});
 	});
+
 };
 
 
